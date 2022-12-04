3a3156b27d563a92caace57f9697844e17ea56e1
devcrazyboy <bnm0612@gmail.com>
2022-12-04 21:49:57 +0900

    강사 로그인 회원가입 중간보고

--- education/src/main/java/com/mobius/education/domain/vo/TeacherVO.java
+++ education/src/main/java/com/mobius/education/domain/vo/TeacherVO.java
@@ -8,4 +8,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class TeacherVO {
+    private Long teacherNumber;
+    private String teacherEmail;
+    private String teacherPassword;
+    private String teacherName;
+    private String teacherBelonging;
+    private String teacherPhoneNumber;
+
+    public void create(String teacherEmail ,String teacherPassword, String teacherName, String teacherBelonging,String teacherPhoneNumber) {
+        this.teacherEmail = teacherEmail;
+        this.teacherPassword = teacherPassword;
+        this.teacherName = teacherName;
+        this.teacherBelonging = teacherBelonging;
+        this.teacherPhoneNumber = teacherPhoneNumber;
+    }
+
 }
--- education/src/main/java/com/mobius/education/repository/TeacherDAO.java
+++ education/src/main/java/com/mobius/education/repository/TeacherDAO.java
@@ -1,5 +1,7 @@
 package com.mobius.education.repository;
 
+import com.mobius.education.domain.vo.TeacherVO;
+import com.mobius.education.domain.vo.UserVO;
 import com.mobius.education.mapper.TeacherMapper;
 import com.mobius.education.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -9,5 +11,11 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class TeacherDAO {
     private final TeacherMapper teacherMapper;
+
+    //    회원가입
+    public void save(TeacherVO teacherVO){teacherMapper.insert(teacherVO);}
+
+    //    로그인
+    public TeacherVO login(TeacherVO teacherVO){return teacherMapper.login(teacherVO);}
 
 }
