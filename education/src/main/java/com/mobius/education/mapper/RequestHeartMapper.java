9d925a49dbed8fba58c37ec2837514944f22a54e
caMelodian <intelliJHLee@gmail.com>
2022-12-06 19:14:06 +0900

    완료

--- education/src/main/java/com/mobius/education/mapper/RequestHeartMapper.java
+++ education/src/main/java/com/mobius/education/mapper/RequestHeartMapper.java
@@ -1,9 +1,11 @@
 package com.mobius.education.mapper;
 
+import com.mobius.education.domain.vo.RequestHeartVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RequestHeartMapper {
     public int selectHeartCount(Long requestNumber);
 
+    public void insert(RequestHeartVO requestHeartVO);
 }
